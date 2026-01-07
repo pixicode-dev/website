266acd696d2ed5a87a539ff20d814b23be357d73
Emric Pichonnier <emric.pichonnier@gmail.com>
2026-01-07 19:30:36 +0100

    fix: ci

--- tools/generate-critical.mjs
+++ tools/generate-critical.mjs
@@ -1,25 +1,40 @@
 import { generate } from "critical";
 
+import fs from "node:fs";
+import path from "node:path";
+
+function getCssFiles(dir) {
+  try {
+    const files = fs.readdirSync(dir);
+    return files
+      .filter((name) => name.endsWith(".css"))
+      .map((name) => path.join(dir, name));
+  } catch (e) {
+    return [];
+  }
+}
+
+const localCssFiles = [
+  ...getCssFiles("public/css/"),
+  ...getCssFiles("public/scss/"),
+];
+
+console.log("🎨 Fichiers CSS locaux trouvés :", localCssFiles);
+
 (async () => {
   await generate({
     base: "public/",
     src: "index.html",
     target: "index.html",
     inline: true,
+    extract: false, // Sécurité
+
+    css: localCssFiles,
 
     dimensions: [
-      {
-        height: 800,
-        width: 375,
-      },
-      {
-        height: 900,
-        width: 1440,
-      },
-      {
-        height: 1080,
-        width: 1920,
-      },
+      { height: 800, width: 375 },
+      { height: 900, width: 1440 },
+      { height: 1080, width: 1920 },
     ],
   });
 })();
